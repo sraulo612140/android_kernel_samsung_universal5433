sraulo612140/android_kernel_samsung_universal5433

commit 77ad50543f85c94b17f21ade6dd769b0a62095eb

8390p: Get rid of init_module/cleanup_module
This trivial patch just "gets rid of" init_module and cleanup_module
from drivers/net/8390p.c

Signed-off-by: Mateusz Mandera <ormi.linux@gmail.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/8390p.c
@@ -91,16 +91,15 @@ void NS8390p_init(struct net_device *dev, int startp)
 }
 EXPORT_SYMBOL(NS8390p_init);
 
-#if defined(MODULE)
-
-int init_module(void)
+static int __init 8390p_init_module(void)
 {
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit 8390p_cleanup_module(void)
 {
 }
 
-#endif /* MODULE */
+module_init(8390p_init_module);
+module_exit(8390p_cleanup_module);
 MODULE_LICENSE("GPL");

commit 7bb30ac1102600c7f216d7a7d37c6308e4298740

8390p: fix build breakage
Fix this build error caused by 77ad50543f85
"8390p: Get rid of init_module/cleanup_module"

drivers/net/8390p.c:94:19: error: invalid suffix "p_init_module" on integer constant
drivers/net/8390p.c:94: error: expected identifier or '(' before numeric constant
drivers/net/8390p.c:99:20: error: invalid suffix "p_cleanup_module" on integer constant

Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/8390p.c
@@ -91,15 +91,15 @@ void NS8390p_init(struct net_device *dev, int startp)
 }
 EXPORT_SYMBOL(NS8390p_init);
 
-static int __init 8390p_init_module(void)
+static int __init NS8390p_init_module(void)
 {
 	return 0;
 }
 
-static void __exit 8390p_cleanup_module(void)
+static void __exit NS8390p_cleanup_module(void)
 {
 }
 
-module_init(8390p_init_module);
-module_exit(8390p_cleanup_module);
+module_init(NS8390p_init_module);
+module_exit(NS8390p_cleanup_module);
 MODULE_LICENSE("GPL");