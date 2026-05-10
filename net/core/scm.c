b7372909aceb8bde958fd72842196e2b6599347b
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-10 15:49:24 +0300

    fix up 67ee5014229f6b984f76e8983d230b41ea51968a https://github.com/salih4187/android_kernel_samsung_universal5433/commit/67ee5014229f6b984f76e8983d230b41ea51968a

--- net/core/scm.c
+++ net/core/scm.c
@@ -46,7 +46,7 @@ static __inline__ int scm_check_creds(struct ucred *creds)
 {
 	const struct cred *cred = current_cred();
 
-    if ((creds->pid == task_tgid_vnr(current)->user_ns || capable(CAP_SYS_ADMIN)) &&
+    if ((creds->pid == task_tgid_vnr(current) || capable(CAP_SYS_ADMIN)) &&
 	    ((creds->uid == cred->uid   || creds->uid == cred->euid ||
 	      creds->uid == cred->suid) || capable(CAP_SETUID)) &&
 	    ((creds->gid == cred->gid   || creds->gid == cred->egid ||
