e7ab373bb2173cfe5a109e1278e7639006a76320
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-16 22:32:43 +0300

    Revert "BACKPORT: random32: add prandom_reseed_late() and call when"
    This reverts commit 8fcb45d2873b09555f78c592d185f2e8844a014e.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -610,11 +610,9 @@ retry:
 
 	r->entropy_total += nbits;
 	if (!r->initialized && nbits > 0) {
-		r->entropy_total += nbits;
 		if (r->entropy_total > 128) {
 			r->initialized = 1;
-			if (r == &nonblocking_pool)
-				prandom_reseed_late();
+			r->entropy_total = 0;
 		}
 	}
 
