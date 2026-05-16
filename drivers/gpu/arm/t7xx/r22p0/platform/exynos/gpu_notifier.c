412b5608d183b1341bb9ab48c420ca228a1930d8
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-17 00:43:38 +0300

    drivers: gpu: r22p0: Guard gpu_noc_nb with CONFIG_EXYNOS_NOC_DEBUGGING

--- drivers/gpu/arm/t7xx/r22p0/platform/exynos/gpu_notifier.c
+++ drivers/gpu/arm/t7xx/r22p0/platform/exynos/gpu_notifier.c
@@ -159,6 +159,7 @@ static int gpu_pm_notifier(struct notifier_block *nb, unsigned long event, void 
 	return err;
 }
 
+#ifdef CONFIG_EXYNOS_NOC_DEBUGGING
 static int gpu_noc_notifier(struct notifier_block *nb, unsigned long event, void *cmd)
 {
 	if (strstr((char *)cmd, "G3D")) {
@@ -167,6 +168,7 @@ static int gpu_noc_notifier(struct notifier_block *nb, unsigned long event, void
 	}
 	return 0;
 }
+#endif /* CONFIG_EXYNOS_NOC_DEBUGGING */
 
 static int gpu_power_on(struct kbase_device *kbdev)
 {
@@ -228,9 +230,11 @@ static struct notifier_block gpu_pm_nb = {
 	.notifier_call = gpu_pm_notifier
 };
 
+#ifdef CONFIG_EXYNOS_NOC_DEBUGGING
 static struct notifier_block gpu_noc_nb = {
 		.notifier_call = gpu_noc_notifier
 };
+#endif /* CONFIG_EXYNOS_NOC_DEBUGGING */
 
 static int gpu_device_runtime_init(struct kbase_device *kbdev)
 {
